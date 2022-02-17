fe42fb88cdad68827b492f85702abf79299ac1c5
lorphemus <bnk@excess.web.tr>
2022-02-17 12:50:07 +0300

    Theme splitted. Menu updated. Logo changed

---
+++ assets/footer.php
@@ -0,0 +1,82 @@
+<footer class="site-footer site-footer-2 clearfix">
+    	<div class="site-footer__main">
+	    	<div class="container">
+	    		<div class="row">
+	    			<div class="col-md-6 col-sm-12 col-xs-12 footer-block">
+			    		<img src="assets/images/footer_logo.png" alt="">
+			    		<p class="address pt-30">Office: 972 Sylvan Street South Angelina, NL S0B2V9<br>
+							Phone: (633) 497-1888 / (062) 109-9222<br>
+							Email: example.com
+						</p>
+			    	</div>
+			    	<div class="col-md-3 col-sm-6 col-xs-12 footer-block footer-1">
+		    			<h3 class="widget-title">Quick Links</h3>
+		    			<ul>
+							<li><a href="index.php">Home</a></li>
+		    				<li><a href="about.php">About Us</a></li>
+		    				<li><a href="products.php">Products</a></li>
+		    				<li><a href="contact.php">Contact</a></li>
+		    			</ul>
+			    	</div>
+			    	
+			    	<div class="col-md-3 col-sm-6 col-xs-12 footer-block footer-3">
+			    		<h3 class="widget-title">OPENING TIME</h3>
+		    			<ul>
+		    				<li>
+		    					<span>Monday :</span>
+		    					<span>9am - 5pm</span>
+		    				</li>
+		    				<li>
+		    					<span>Tuesday :</span>
+		    					<span>9am - 1pm</span>
+		    				</li>
+		    				<li>
+		    					<span>Wendsday :</span>
+		    					<span>9am - 5pm</span>
+		    				</li>
+		    				<li>
+		    					<span>Thursday :</span>
+		    					<span>9am - 1pm</span>
+		    				</li>
+		    				<li>
+		    					<span>Friday :</span>
+		    					<span>9am - 1pm</span>
+		    				</li>
+		    				<li>
+		    					<span>Saturday - Sunday :</span>
+		    					<span>closed</span>
+		    				</li>
+		    			</ul>
+			    	</div>
+			    </div>
+		    </div>
+	   </div>
+	   <div class="site-footer__copyright">
+	   		<div class="container">
+	   			<div class="copyright_container">
+	   				<div class="col-md-4 col-sm-6">
+			   			<ul class="social">
+							<li>
+								<a href="#"><i class="fa fa-instagram"></i></a>
+							</li>
+							<li>
+								<a href="#"><i class="fa fa-facebook"></i></a>
+							</li>
+							<li>
+								<a href="#"><i class="fa fa-twitter"></i></a>
+							</li>
+							<li>
+								<a href="#"><i class="fa fa-youtube"></i></a>
+							</li>
+						</ul>
+					</div>
+		    		<div class="copyright col-md-4 col-sm-6">
+		        		© <?php echo date("Y"); ?>. All rights reserved.<a href="https://wwww.excessreklam.com" target="_blank">Excess Reklam</a>
+		        	</div>
+		        	<div class="payment col-md-4 col-sm-12">
+		        		<img src="assets/images/payment.png" alt="">
+		        	</div>
+	        	</div>
+	       </div>
+	   </div>
+	</footer>
---
+++ assets/header.php
@@ -0,0 +1,189 @@
+<header class="header home2">
+		<!-- top-bar -->
+		<div class="topbar">
+			<div class="container">
+				<div class="top-bar">
+			        <div class="top-bar__left pull">
+			        	<div class="top-bar-register">
+				            <a class="top-bar__item" href="account.html">
+				            	<i class="fa fa-user-o" aria-hidden="true"></i>
+				            	Register
+				            </a>
+				        </div>
+			            <div class="top-bar-account">
+				            <a class="top-bar__item" href="#">
+				            	<i class="fa fa-lock" aria-hidden="true"></i>
+				            	login
+				            </a>
+			            	<div class="content-dropdown left">
+								<div class="account-inner ">
+								    <div class="login-form-head">
+								      	<span class="login-form-title">Sign in</span>
+						      	<span class="pull-right">
+								     	 	<a class="register-link" href="account.html" title="Register">Create an Account</a>
+								     	 </span>
+								    </div>
+								    <form class="opal-login-form-ajax" data-toggle="validator">
+								        <p>
+								        <label>Username or email <span class="required">*</span></label>
+								        <input name="username" type="text" required="" placeholder="Username">
+								        </p>
+								        <p>
+								        <label>Password <span class="required">*</span></label>
+								        <input name="password" type="password" required="" placeholder="Password">
+								        </p>
+								        <button type="submit" data-button-action="" class="btn btn-primary btn-block">Login</button>
+								        <input type="hidden" name="action" value="">
+								        <input type="hidden" id="security-login" name="security-login" value="">
+								        <input type="hidden" name="login" value="">
+								    </form>
+								    <div class="login-form-bottom">
+								      		<a href="#" class="lostpass-link" title="Lost your password?">Lost your password?</a>
+								    </div>          
+								</div>
+							</div>     
+			        	</div>
+			        </div>
+			        <div class="top-bar__right">
+			        	<div class="header-language item-dropdown">
+							<a href="#" class="top-bar__item">English
+								<i class="fa fa-angle-down"></i>
+							</a>
+							<ul class="content-dropdown">
+								<li class="active"><a href="#">English</a></li>
+								<li><a href="#">German</a></li>
+								<li><a href="#">Turkish</a></li>
+							</ul>
+						</div>
+					</div>
+		        </div>
+			</div>
+			<!-- /Container -->
+	    </div>
+	    <!-- /top-bar -->
+
+	    <!-- header-main -->
+	    <div class="header-main pb-30">
+	    	<div class="container">
+			    <nav id="nav" class="navbar">
+			    	<div class="nav-collapse">
+						<span></span>
+					</div>
+					<div class="navbar-header">
+						<!-- Logo -->
+						<div class="navbar-brand">
+							<a class="logo" href="index.html"><img src="assets/images/logo-3.png" alt=""></a> 
+						</div>
+						<!-- /Logo -->
+					</div>
+					<!--  Main navigation  -->
+
+					<div class="otf-flex-item"></div>
+					
+					<ul class="main-nav nav navbar-nav navbar-right">
+						<li><a href="index.php">Home</a></li>
+			         	<li><a href="about.php">About Us</a></li>
+			           	<li><a href="products.php">Products</a></li></li>
+			            <li><a href="contact.php">Contact</a></li>
+					</ul>
+					<!-- /Main navigation -->
+					<div class="otf-flex-item"></div>
+					<div class="site-header -icon">
+						<!-- search -->
+							<div class="site-header__search">
+				            <div class="ps-search-btn">
+				              	<i class="fa fa-search" aria-hidden="true"></i>
+				            </div>
+				            <div class="ps-search">
+						     <div class="ps-search__content">
+						     	<a class="ps-search__close" href="#"><span></span></a>
+						     	<h3 class="search_title">Enter your keyword</h3>
+						        <form method="get" class="searchform" action="#">
+									<div class="wiget-search input-group">
+									 <input name="s" maxlength="40" class="form-control input-search" type="text" size="20" placeholder="Searching..."> 
+
+									 <span class="input-group-addon input-large btn-search"> 
+									 	<span class="fa fa-search"></span>
+										<input type="submit" class="fa" value=""> 
+									 </span>
+									</div>
+								</form>
+						      </div>
+						    </div>
+						</div>
+					</div>
+				</nav>
+	    	</div>
+	    </div>
+	    <!-- /header-main -->
+	</header>
+		<!--/header-desktop -->
+
+		<!-- header-mobile -->
+	<div class="header-mobile home2">
+		<div class="header-mobile-top">
+	        <div class="top-bar">
+				<div class="header-language item-dropdown">
+					<a href="#" class="top-bar__item">English
+						<i class="fa fa-angle-down"></i>
+					</a>
+					<ul class="content-dropdown">
+						<li class="active"><a href="#">English</a></li>
+						<li><a href="#">German</a></li>
+						<li><a href="#">Turkish</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="site-header -icon">
+				<!-- search -->
+					<div class="site-header__search">
+			            <div class="ps-search-btn">
+			              	 <i class="fa fa-search" aria-hidden="true"></i>
+			            </div>
+			            <div class="ps-search">
+						    <div class="ps-search__content">
+						     	<a class="ps-search__close" href="#"><span></span></a>
+						     	<h3 class="search_title">Enter your keyword</h3>
+						        <form method="get" class="searchform" action="#">
+									<div class="wiget-search input-group">
+									 <input name="s" maxlength="40" class="form-control input-search" type="text" size="20" placeholder="Searching..."> 
+
+									 <span class="input-group-addon input-large btn-search"> 
+									 	<span class="fa fa-search"></span>
+									 	<input type="submit" class="fa" value=""> 
+									 </span>
+									</div>
+								</form>
+						    </div>
+					    </div>
+					</div>
+				<!--end search -->
+		    </div>
+	    </div>
+		
+		<nav class="navbar">
+		    <div class="navbar-header">
+		      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+		        <span class="sr-only">Toggle navigation</span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		      </button>
+		      <!-- logo -->
+		      <div class="navbar-brand">
+		      	<a class="logo" href="index.php"><img src="assets/images/logo-2.png" alt=""> </a> 
+		      </div>
+		      <!-- logo -->
+		    </div>
+
+		    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		      	<ul class="nav navbar-nav">
+			        <li><a href="index.php">Home</a></li>
+			        <li><a href="about.php">About Us</a></li>
+                    <li><a href="products.php">Products</a></li>
+		       		<li><a href="contact.php">Contact</a></li>
+		      	</ul>
+		    </div>
+		</nav>
+	</div>
+	
